4a965f488e1ca34b801edb5134fdaba7b13a115b
liupengkun <liupengkun@360.cn>
2025-03-12 15:51:31 +0800

    update

--- latentsync/pipeline.py
+++ latentsync/pipeline.py
@@ -240,13 +240,14 @@ class Pipeline(LipsyncPipeline):
         decoded_latents = self.paste_surrounding_pixels_back(
             decoded_latents, pixel_values, 1 - masks, device, weight_dtype
         )
+        synced_video_frames.append(decoded_latents)
         # masked_video_frames.append(masked_pixel_values)
 
         end_time = time.perf_counter()
         print(f"推理后处理前半段执行时间: {end_time - start_time:.6f} 秒")
 
         synced_video_frames = self.restore_video(
-            torch.cat(decoded_latents), original_video_frames, boxes, affine_matrices
+            torch.cat(synced_video_frames), original_video_frames, boxes, affine_matrices
         )
         # masked_video_frames = self.restore_video(
         #     torch.cat(masked_video_frames), original_video_frames, boxes, affine_matrices
