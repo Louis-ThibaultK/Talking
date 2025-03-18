daa2e6b4fcf2b7bf48bee53fc4cfa06f4d76c4d6
liupengkun <liupengkun@360.cn>
2025-03-18 20:59:50 +0800

    2

--- webrtc.py
+++ webrtc.py
@@ -29,11 +29,11 @@ from av import AudioFrame
 import fractions
 import numpy as np
 
-AUDIO_PTIME = 0.040  # 20ms audio packetization
+AUDIO_PTIME = 0.020  # 20ms audio packetization
 VIDEO_CLOCK_RATE = 90000
 VIDEO_PTIME = 1 / (12.5)  # 30fps
 VIDEO_TIME_BASE = fractions.Fraction(1, VIDEO_CLOCK_RATE)
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 48000
 AUDIO_TIME_BASE = fractions.Fraction(1, SAMPLE_RATE)
 
 #from aiortc.contrib.media import MediaPlayer, MediaRelay
